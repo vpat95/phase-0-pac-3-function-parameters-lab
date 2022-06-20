8300e41a41ab76c6293e51e7b886fd2c486246f1
Vivek Patel <vpatel360@gmail.com>
2022-06-20 02:25:56 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction("Aki");
+introduction("Samip");
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Aki", "Ember.js");
+introductionWithLanguage("Samip", "React");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional("Gracie", "Javascript");
+introductionWithLanguageOptional("Gracie", "Python");
